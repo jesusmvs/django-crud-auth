3868bbb6478423f2ccd145649e5ae050c6b09391
jesusmvs <jesusmvs10@gmail.com>
2025-02-25 18:55:18 -0300

    fix settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.        
-        default='postgresql://postgres:postgres@localhost:5432/django-crud-auth',        
+        default='postgresql://postgres:postgres@localhost:5432/django-crud',        
         conn_max_age=600    
     )
 }
